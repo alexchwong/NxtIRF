55b06df81e02943bd228736217eba3cd9cc8217a
alexw-gsct <80015046+alexchwong@users.noreply.github.com>
2021-05-26 14:49:42 +1000

    Update BAMReader_Multi.cpp

--- src/BAMReader_Multi.cpp
+++ src/BAMReader_Multi.cpp
@@ -527,9 +527,11 @@ int BAMReader_Multi::read_from_file(unsigned int n_blocks) {
     // Set begin cursor if BAM_BLOCK_CURSOR == begin_block_offset
     if(BAM_BLOCK_CURSOR == begin_block_offset) {
       buffer.at(comp_buffer_count).SetPos(begin_read_offset);
+      Rcout << "Block @ " << begin_block_offset << " Setting begin read offset" << begin_read_offset << '\n';
     }
     if(BAM_BLOCK_CURSOR == end_block_offset && end_block_offset > 0) {
       buffer.at(comp_buffer_count).SetEndPos(end_read_offset);
+      Rcout << "Block @ " << end_block_offset << " Setting end read offset" << end_block_offset << '\n';
       IS_EOF = 1;   // Set virtual EOF
       i--;
     }
